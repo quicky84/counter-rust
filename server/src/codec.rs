6c6baa258466379143c05d67ec2e5de8be69e425
Victor Emolaev <ve@ve-secman.local.nedap>
2017-11-24 16:58:28 +0100

    Get rid of tokio-proto

---
+++ server/src/codec.rs
@@ -0,0 +1,84 @@
+use atoi::atoi;
+use bytes::BytesMut;
+use std::io;
+use tokio_io::codec::{Encoder, Decoder};
+
+pub enum Completion {
+    Time(u64), // milliseconds
+    OutOfTime,
+}
+
+#[derive(Debug)]
+pub struct Request {
+    pub id: u32,
+    pub difficulty: u32,
+}
+
+pub struct Response {
+    pub id: u32,
+    pub completion: Completion,
+}
+
+#[derive(Debug)]
+pub struct TaskCodec;
+
+impl TaskCodec {
+    pub fn new() -> Self {
+        TaskCodec
+    }
+}
+
+impl Decoder for TaskCodec {
+    type Item = Request;
+    type Error = io::Error;
+
+    fn decode(&mut self, buf: &mut BytesMut) -> io::Result<Option<Request>> {
+        println!("Received: {:?}", buf);
+
+        // Expected input is `u32 u32\n`
+        let i = match buf.iter().position(|&b| b == b'\n') {
+            Some(i) => i,
+            _ => return Ok(None),
+        };
+
+        // read up the first `\n`
+        let sub_buf = buf.split_to(i);
+
+        // after the read-out, there is still `\n` belogning to "our" input which has to be removed
+        buf.split_to(1);
+
+        let i = match sub_buf.iter().position(|&b| b == b' ') {
+            Some(i) => i,
+            _ => return Ok(None),
+        };
+
+        let (id, difficulty) = (&sub_buf[..i], &sub_buf[i + 1..]);
+
+        let (id, difficulty) = match (atoi::<u32>(id), atoi::<u32>(difficulty)) {
+            (Some(id), Some(difficulty)) => (id, difficulty),
+            _ => return Ok(None),
+        };
+
+        println!("\tParsed:\n\tTask {}\n\tdifficulty: {}", id, difficulty);
+
+        Ok(Some(Request { id, difficulty }))
+    }
+}
+
+impl Encoder for TaskCodec {
+    type Item = Response;
+    type Error = io::Error;
+
+    fn encode(&mut self, res: Response, buf: &mut BytesMut) -> io::Result<()> {
+        let msg = match res.completion {
+            Completion::Time(t) => format!("Task {} completed in {} milliseconds", res.id, t),
+            Completion::OutOfTime => format!("{} ran out of time", res.id),
+        };
+
+        buf.extend(msg.as_bytes());
+        buf.extend(b"\r\n");
+        println!("Task {}: Response: {:?}", res.id, buf);
+
+        Ok(())
+    }
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -2,171 +2,44 @@
 #![cfg_attr(feature = "dev", feature(plugin))]
 #![cfg_attr(feature = "dev", plugin(clippy))]
 
-extern crate counter_server;
-
-extern crate tokio_proto;
+extern crate futures;
 extern crate tokio_core;
 extern crate tokio_io;
-extern crate tokio_service;
-
-extern crate futures;
-extern crate futures_cpupool;
 extern crate bytes;
-
 extern crate atoi;
-
+extern crate futures_cpupool;
 extern crate num_cpus;
+extern crate counter_server;
 
-use atoi::atoi;
-use bytes::BytesMut;
+mod codec;
+
+use codec::{TaskCodec, Response, Completion};
 use counter_server::Config;
-
-use futures::Future;
+use futures::{Future, Stream, Sink};
 use futures_cpupool::CpuPool;
-use std::cmp;
-
-use std::env;
-
-use std::io;
-use std::net::{IpAddr, Ipv4Addr};
-
-use std::net::SocketAddr;
-use std::process;
+use std::{cmp, env, process};
+use std::net::{SocketAddr, IpAddr, Ipv4Addr};
 use std::time::Instant;
-use tokio_io::{AsyncRead, AsyncWrite};
-use tokio_io::codec::{Encoder, Decoder, Framed};
-use tokio_proto::TcpServer;
-use tokio_proto::pipeline::ServerProto;
-use tokio_service::Service;
+use tokio_core::net::TcpListener;
+use tokio_core::reactor::Core;
+use tokio_io::AsyncRead;
 
 
-pub enum Completion {
-    Time(u64), // milliseconds
-    OutOfTime,
-    Err(u8),
-}
+fn compute(difficulty: u64, timeout: u64) -> Completion {
+    let now = Instant::now();
 
-pub struct Request {
-    id: u32,
-    difficulty: u32,
-}
-
-pub struct Response {
-    id: u32,
-    completion: Completion,
-}
-
-pub struct TaskCodec;
-
-impl Decoder for TaskCodec {
-    type Item = Request;
-    type Error = io::Error;
-
-    fn decode(&mut self, buf: &mut BytesMut) -> io::Result<Option<Request>> {
-        println!("Received: {:?}", buf);
-
-        // Expected input is `u32 u32\n`
-        let i = match buf.iter().position(|&b| b == b'\n') {
-            Some(i) => i,
-            _ => return Ok(None),
-        };
-
-        // read up the first `\n`
-        let sub_buf = buf.split_to(i);
-
-        // after the read-out, there is still `\n` belogning to "our" input which has to be removed
-        buf.split_to(1);
-
-        let i = match sub_buf.iter().position(|&b| b == b' ') {
-            Some(i) => i,
-            _ => return Ok(None),
-        };
-
-        let (id, difficulty) = (&sub_buf[..i], &sub_buf[i + 1..]);
-
-        let (id, difficulty) = match (atoi::<u32>(id), atoi::<u32>(difficulty)) {
-            (Some(id), Some(difficulty)) => (id, difficulty),
-            _ => return Ok(None),
-        };
-
-        println!("\tParsed:\n\tTask {}\n\tdifficulty: {}", id, difficulty);
-
-        Ok(Some(Request { id, difficulty }))
-    }
-}
-
-impl Encoder for TaskCodec {
-    type Item = Response;
-    type Error = io::Error;
-
-    fn encode(&mut self, res: Response, buf: &mut BytesMut) -> io::Result<()> {
-        let msg = match res.completion {
-            Completion::Time(t) => format!("Task {} completed in {} milliseconds", res.id, t),
-            Completion::OutOfTime => format!("{} ran out of time", res.id),
-            Completion::Err(e) => format!("{} errored with:\n{}", res.id, e),
-        };
-
-        buf.extend(msg.as_bytes());
-        buf.extend(b"\r\n");
-        println!("Task {}: Response: {:?}", res.id, buf);
-
-        Ok(())
-    }
-}
-
-pub struct TaskProto;
-
-impl<T: AsyncRead + AsyncWrite + 'static> ServerProto<T> for TaskProto {
-    type Request = Request;
-    type Response = Response;
-    type Transport = Framed<T, TaskCodec>;
-    type BindTransport = io::Result<Framed<T, TaskCodec>>;
-
-    fn bind_transport(&self, io: T) -> io::Result<Framed<T, TaskCodec>> {
-        Ok(io.framed(TaskCodec))
-    }
-}
-
-pub struct CountingService {
-    pub thread_pool: CpuPool,
-    pub timeout: u64,
-}
-
-impl Service for CountingService {
-    type Request = Request;
-    type Response = Response;
-
-    type Error = io::Error;
-    type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
-
-    // Produce a future for computing a response from a request.
-    fn call(&self, req: Self::Request) -> Self::Future {
-        let (id, difficulty) = (req.id, u64::from(req.difficulty));
-
-        fn compute(difficulty: u64, timeout: u64) -> Completion {
-            let now = Instant::now();
-            for _ in 0..difficulty {
-                let elapsed = now.elapsed();
-                if elapsed.as_secs() > timeout {
-                    return Completion::OutOfTime;
-                }
-            }
-            let elapsed = now.elapsed();
-            let millisec = (elapsed.as_secs() * 1_000) +
-                u64::from(elapsed.subsec_nanos() / 1_000_000);
-            Completion::Time(millisec)
+    for _ in 0..difficulty {
+        let elapsed = now.elapsed();
+        if elapsed.as_secs() > timeout {
+            return Completion::OutOfTime;
         }
-
-        let timeout = self.timeout;
-        let computation = self.thread_pool.spawn_fn(move || {
-            Ok(compute(difficulty, timeout))
-        });
-
-        Box::new(computation.map(move |completion| {
-            println!("Task {} completed", id);
-            Response { id, completion }
-        }))
     }
+
+    let elapsed = now.elapsed();
+
+    let millisec = (elapsed.as_secs() * 1_000) + u64::from(elapsed.subsec_nanos() / 1_000_000);
+
+    Completion::Time(millisec)
 }
 
 fn main() {
@@ -178,19 +51,52 @@ fn main() {
     });
 
     let cpus = cmp::min(num_cpus::get(), config.n_kernels);
-    let thread_pool = CpuPool::new(cpus);
+    let pool = CpuPool::new(cpus);
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), config.port);
 
     if let IpAddr::V4(ip) = addr.ip() {
         println!("Running on {:?}:{:?}", ip, addr.port());
     }
 
-    let server = TcpServer::new(TaskProto, addr);
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
 
-    server.serve(move || {
-        Ok(CountingService {
-            thread_pool: thread_pool.clone(),
-            timeout: config.timeout,
-        })
+    let listener = TcpListener::bind(&addr, &handle).unwrap();
+
+    let connections = listener.incoming();
+
+    let server = connections.for_each(|(client, _peer_address)| {
+        //     sink  stream
+        let (writer, reader) = client.framed(TaskCodec::new()).split();
+
+        let pool_clone = pool.clone();
+        let timeout = config.timeout;
+
+        let responses = reader.and_then(move |rq| {
+            let (id, difficulty) = (rq.id, u64::from(rq.difficulty));
+            let copy_id = id;
+
+            let computation = pool_clone.spawn_fn(move || {
+                println!("--------> Starting Task {}", copy_id);
+
+                let completion = compute(difficulty, timeout);
+
+                println!("<-------- Finishing Task {}", copy_id);
+
+                Ok(completion)
+            });
+
+            Box::new(computation.map(move |completion| {
+                println!("Task {} completed", id);
+                Response { id, completion }
+            }))
+        });
+
+        let handler = writer.send_all(responses).then(|_| Ok(()));
+        handle.spawn(handler);
+
+        Ok(())
     });
+
+    core.run(server).unwrap();
 }
